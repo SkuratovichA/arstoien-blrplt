ccc4acdafbdb662e40b2706b2a9bf5182b35c741
SkuratovichA <skuratovich.aliaksandr@gmail.com>
2025-12-05 23:01:18 +0100

    configure actions

--- packages/client/codegen.ts
+++ packages/client/codegen.ts
@@ -2,7 +2,7 @@ import { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: 'http://localhost:4000/graphql',
+  schema: process.env.CI ? './schema.graphql' : 'http://localhost:4000/graphql',
   documents: ['src/**/*.graphql.ts'],
   ignoreNoDocuments: true,
   generates: {
