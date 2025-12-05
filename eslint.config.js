486b8bf721198cb67df7d4ced0f8b8b7ee3cb7e5
SkuratovichA <skuratovich.aliaksandr@gmail.com>
2025-12-05 21:52:01 +0100

    eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,21 @@
 const { eslintrc } = require('@arstoien/devtools');
 
-module.exports = eslintrc;
+module.exports = [
+  ...eslintrc,
+  {
+    ignores: [
+      '**/dist/**',
+      '**/node_modules/**',
+      '**/*.config.js',
+      '**/*.config.cjs',
+      '**/*.config.ts',
+      '**/codegen.ts',
+      '**/vite.config.ts',
+      '**/tailwind.config.ts',
+      '**/postcss.config.js',
+      '**/i18next-parser.config.cjs',
+      '**/.eslintrc.js',
+      '**/.eslintrc.cjs'
+    ]
+  }
+];
